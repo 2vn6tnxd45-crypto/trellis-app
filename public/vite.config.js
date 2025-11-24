4ca3d2533a52ff2c6046c0fa475a58aaf77119ad
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2025-11-23 19:45:47 -0800

    Create vite.config.js

---
+++ public/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app/index.html'),
+        report: resolve(__dirname, 'public/pedigree_report.html')
+      },
+    },
+  },
+})
