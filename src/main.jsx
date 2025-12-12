487388966ebc7a914819196ac3a38156aa2aeb37
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2025-12-11 20:34:55 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'  <-- DELETE THIS LINE OR COMMENT IT OUT
+// import './index.css' // Removed standard css as we use krib-theme now
+import './styles/krib-theme.css' // Import the new theme system
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
