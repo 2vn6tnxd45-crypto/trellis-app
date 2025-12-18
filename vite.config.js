e98f6ee8ad30b6771d240c614d00a856b0fb6598
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2025-12-17 22:20:41 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve, dirname } from 'path' // Import dirname
-import { fileURLToPath } from 'url'     // Import fileURLToPath
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-// Define __dirname manually for ES Modules
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext', // <--- ADD THIS LINE
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         app: resolve(__dirname, 'app/index.html'),
-        // report: resolve(__dirname, 'public/pedigree_report.html') // Remove this (see point 3)
       },
     },
   },
