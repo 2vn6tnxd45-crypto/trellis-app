438f2cb1d4e346b3da1306b5a1e45755d04d5ba2
Devon Davila <devondavila@Mac-4861.lan>
2025-12-29 19:50:50 -0800

    add comment

--- api/neighborhood.js
+++ api/neighborhood.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
     const latFixed = Number(lat).toFixed(4);
     const lonFixed = Number(lon).toFixed(4);
 
+    //jijdioejdijeiodj
+
     try {
         // --- Execute All Fetches in Parallel ---
 
