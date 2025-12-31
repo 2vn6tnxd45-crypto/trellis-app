359162d6b5f2aa2311d630a6c7107b9e0403b71d
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2025-12-30 22:16:09 -0800

    Update useThemeInit.js

--- src/hooks/useThemeInit.js
+++ src/hooks/useThemeInit.js
@@ -1,6 +1,11 @@
+// src/hooks/useThemeInit.js
+import { useEffect } from 'react';
+
 export const useThemeInit = () => {
     useEffect(() => {
         document.documentElement.classList.remove('dark');
         localStorage.setItem('krib-theme', 'light');
     }, []);
 };
+
+export default useThemeInit;
