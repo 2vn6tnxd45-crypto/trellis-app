790f22710a8a29b006b0a09700e749e9cb8a3038
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2025-12-31 16:56:34 -0800

    Create index.js

---
+++ src/features/report/components/index.js
@@ -0,0 +1,6 @@
+// src/features/report/components/index.js
+// Export all report components
+
+export { PropertyValueCard } from './PropertyValueCard';
+export { WarrantyTimeline } from './WarrantyTimeline';
+export { InvestmentBreakdown } from './InvestmentBreakdown';
