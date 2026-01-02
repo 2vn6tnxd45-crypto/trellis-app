893cd7ca73a7b619162f8d55c1b9bbc7e12ae09e
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-01 16:35:32 -0800

    Create index.js

---
+++ src/features/quotes/index.js
@@ -0,0 +1,44 @@
+// src/features/quotes/index.js
+// ============================================
+// QUOTES FEATURE - EXPORTS
+// ============================================
+
+// Components
+export { QuotesListView, QuoteStatusBadge } from './components/QuotesListView';
+export { QuoteBuilder } from './components/QuoteBuilder';
+export { QuoteDetailView } from './components/QuoteDetailView';
+export { PublicQuoteView } from './components/PublicQuoteView';
+
+// Hooks
+export { 
+    useQuotes,
+    useQuoteTemplates,
+    useQuoteStats,
+    useQuoteOperations,
+    useQuoteManagement
+} from './hooks/useQuotes';
+
+// Service functions
+export {
+    generateQuoteNumber,
+    createQuote,
+    updateQuote,
+    deleteQuote,
+    getQuote,
+    getQuotes,
+    subscribeToQuotes,
+    sendQuote,
+    markQuoteViewed,
+    acceptQuote,
+    declineQuote,
+    checkExpiredQuotes,
+    createQuoteTemplate,
+    getQuoteTemplates,
+    subscribeToQuoteTemplates,
+    deleteQuoteTemplate,
+    linkQuoteToInvoice,
+    linkQuoteToJob,
+    getQuoteStats,
+    getQuoteByShareToken,
+    generateQuoteShareLink
+} from './lib/quoteService';
