7e0d425bf2ac2f80287fd0617eb345c35223d29c
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-03 23:06:43 -0800

    Create index.js

---
+++ src/features/jobs/components/completion/index.js
@@ -0,0 +1,11 @@
+// src/features/jobs/components/completion/index.js
+// ============================================
+// JOB COMPLETION COMPONENTS - BARREL EXPORT
+// ============================================
+
+export { JobCompletionForm } from './JobCompletionForm';
+export { JobCompletionReview } from './JobCompletionReview';
+
+// Future components can be added here:
+// export { ItemImportPreview } from './ItemImportPreview';
+// export { PartialCompletionModal } from './PartialCompletionModal';
