7ac9df8117113153513c83dd4b4866e84cb5d4e5
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-03 21:51:03 -0800

    Create index.js

---
+++ src/features/ratings/index.js
@@ -0,0 +1,13 @@
+// src/features/ratings/index.js
+// ============================================
+// RATINGS FEATURE - PUBLIC EXPORTS
+// ============================================
+
+// Components
+export { StarRating, RatingDisplay, CategoryRatingInput } from './components/StarRating';
+export { RateContractorModal } from './components/RateContractorModal';
+export { RateHomeownerModal } from './components/RateHomeownerModal';
+
+// Future exports:
+// export { ContractorRatingsList } from './components/ContractorRatingsList';
+// export { RatingSummaryCard } from './components/RatingSummaryCard';
