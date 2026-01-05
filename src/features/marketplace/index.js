f95430ec3806b7877a8e883e56f122410af93c6f
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-04 21:36:37 -0800

    Create index.js

---
+++ src/features/marketplace/index.js
@@ -0,0 +1,14 @@
+// src/features/marketplace/index.js
+// ============================================
+// MARKETPLACE FEATURE - PUBLIC EXPORTS
+// ============================================
+
+// Services
+export * from './lib/serviceRequestService';
+export * from './lib/contractorMarketplaceService';
+
+// Components
+export { ServiceRequestCreator } from './components/ServiceRequestCreator';
+export { ContractorLeadDashboard } from './components/ContractorLeadDashboard';
+export { ContractorBrowser } from './components/ContractorBrowser';
+export { HomeownerRequestManager } from './components/HomeownerRequestManager';
