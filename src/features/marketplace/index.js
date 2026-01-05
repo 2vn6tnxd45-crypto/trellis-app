24163506fe69a5ec4e52f0398880756d207ee711
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-04 21:42:54 -0800

    Update index.js

--- src/features/marketplace/index.js
+++ src/features/marketplace/index.js
@@ -7,8 +7,8 @@
 export * from './lib/serviceRequestService';
 export * from './lib/contractorMarketplaceService';
 
-// Components
-export { ServiceRequestCreator } from './components/ServiceRequestCreator';
-export { ContractorLeadDashboard } from './components/ContractorLeadDashboard';
-export { ContractorBrowser } from './components/ContractorBrowser';
-export { HomeownerRequestManager } from './components/HomeownerRequestManager';
+// Components (these use default exports, so we import and re-export as named)
+export { default as ServiceRequestCreator } from './components/ServiceRequestCreator';
+export { default as ContractorLeadDashboard } from './components/ContractorLeadDashboard';
+export { default as ContractorBrowser } from './components/ContractorBrowser';
+export { default as HomeownerRequestManager } from './components/HomeownerRequestManager';
