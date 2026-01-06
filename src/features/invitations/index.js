2909b792c4650c4605844af04125f99e0d4ea580
2vn6tnxd45-crypto <2vn6tnxd45@privaterelay.appleid.com>
2026-01-05 17:23:33 -0800

    Update index.js

--- src/features/invitations/index.js
+++ src/features/invitations/index.js
@@ -1,8 +1,10 @@
 // src/features/invitations/index.js
 
+// src/features/invitations/index.js
 export { ContractorInviteCreator } from './ContractorInviteCreator';
 export { InvitationClaimFlow } from './InvitationClaimFlow';
 export { ContractorLanding } from './ContractorLanding';
+export { ComparisonPage } from './ComparisonPage';
 
 // Re-export utility functions
 export { 
